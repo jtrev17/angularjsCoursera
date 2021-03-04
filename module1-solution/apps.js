a931842c89da2bbb9ce7ecd156a14c9bf745762d
Jennifer Trevino <jntrev17dev@gmail.com>
2021-03-04 11:32:46 -0600

    changing messagelunch

--- module1-solution/apps.js
+++ module1-solution/apps.js
@@ -7,26 +7,11 @@
             function LunchCheckController($scope) {
                         $scope.lunch="";
                         $scope.messageLunch = "";
-                        $scope.styleMessage = "";
                         $scope.itemsLunch = function () {
                                     var items = itemsForLunch($scope.lunch);
                                     $scope.messageLunch = items;
                         }
-                        $scope.myStyle = {
-                                    message: [
-                                                {
-                                                            'green' : messageLunch === 'Enjoy!'
-                                                },
-                                                {
-                                                            'yellow' : messageLunch === 'Too much!'
-                                                },
-                                                {
-                                                            'red' : messageLunch === 'Please enter your lunch'
-                                                            
-                                                }
-                                    ]
-                        }
-
+                        
                         function itemsForLunch(string) {
                                     var message = "";
                                     var luncharray = string.split(",");
