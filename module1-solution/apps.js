df397e45e57aa6ea1706d9494df0b70724e74835
Jennifer Trevino <jntrev17dev@gmail.com>
2021-03-04 11:15:30 -0600

    Solution 1 enhanced

--- module1-solution/apps.js
+++ module1-solution/apps.js
@@ -7,25 +7,40 @@
             function LunchCheckController($scope) {
                         $scope.lunch="";
                         $scope.messageLunch = "";
+                        $scope.styleMessage = "";
                         $scope.itemsLunch = function () {
                                     var items = itemsForLunch($scope.lunch);
                                     $scope.messageLunch = items;
+                        }
+                        $scope.myStyle = {
+                                    message: [
+                                                {
+                                                            'green' : messageLunch === 'Enjoy!'
+                                                },
+                                                {
+                                                            'yellow' : messageLunch === 'Too much!'
+                                                },
+                                                {
+                                                            'red' : messageLunch === 'Please enter your lunch'
+                                                            
+                                                }
+                                    ]
                         }
 
                         function itemsForLunch(string) {
                                     var message = "";
                                     var luncharray = string.split(",");
                                     if(luncharray.length  <= 3 && luncharray != ''){
-                                                message = "Enjoy!"
+                                                message = "Enjoy!";
                                     } else if (luncharray.length > 3) {
                                                 message = "Too much!";
                                     } else if (luncharray == ''){
                                                 message = "Please enter your lunch";
                                     }
 
-                                    return message;
-                                    
+                                    return message;  
                         }
+                        
             }
             
 })();
